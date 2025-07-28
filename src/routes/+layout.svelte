67db2d10120cfb54132c7d809c903d1c895ab1e7
Aditya Kakarla <85147239+adityakakarla@users.noreply.github.com>
2025-07-27 21:57:07 -0700

    initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import '../app.css';
+
+	let { children } = $props();
+</script>
+
+<div class='bg-stone-900 flex flex-col items-center pt-30 px-10 min-h-screen text-white w-full'>
+	{@render children()}
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,21 @@
+<script>
+</script>
+
+<div class='flex flex-col space-y-2 text-left items-center justify-center max-w-[800px] w-[90vw]'>
+<h1 class='text-2xl font-bold w-full'>Aditya Kakarla</h1>
+<p class='w-full'>Software engineer interested in hard problems with impact 🇺🇸</p>
+<p class='w-full'>Currently deploying endpoint security products for critical institutions @ Microsoft</p>
+<p class='w-full'>Will be taking a gap quarter to build AI tools for the American government @ Palantir</p>
+<h1 class='text-lg font-bold w-full mt-8'>Previously</h1>
+<ul class='list-decimal w-full pl-8'>
+    <li>Worked at USDA and DoD, where I worked on cool projects (ex: feeding more hungry children)</li>
+    <li>Spent 7 months building a coding agent to accelerate cancer genomics research at the Mesirov Lab</li>
+    <li>Designed and built data pipelines to drive millions in revenue for customers at Cadre AI</li>
+</ul>
+<h1 class='text-lg font-bold w-full mt-8'>Contact</h1>
+<p class='w-full'>Always open to meeting people. Currently based in Seattle metro area.</p>
+<div class='flex flex-row space-x-3 w-full'>
+<p><a href="https://www.linkedin.com/in/aditya-kakarla/" class="underline hover:text-blue-400">LinkedIn</a></p>
+<p><a href="https://github.com/adityakakarla" class="underline hover:text-blue-400">GitHub</a></p>
+</div>
+</div>
