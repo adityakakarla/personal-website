a412abb84f1a6dfab27a06577d8826aeede5173d
Aditya Kakarla <85147239+adityakakarla@users.noreply.github.com>
2025-07-27 21:58:20 -0700

    added title

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,6 +4,10 @@
 	let { children } = $props();
 </script>
 
+<svelte:head>
+	<title>Adi's Website</title>
+</svelte:head>
+
 <div class='bg-stone-900 flex flex-col items-center pt-30 px-10 min-h-screen text-white w-full'>
 	{@render children()}
 </div>
